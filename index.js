2d6b029cfec3075aade5b0bcb26d6d1d6a75647c
arthur andrade <arthurandrad.23@gmail.com>
2024-06-03 20:25:59 -0300

    Feat: Worked on creating users handling and installed dependencies

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
 const app = express();
-const port = 3000;
+app.use(express.json());
+app.use(cors({ origin: true }));
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const r = await axios.put(
+      'https://api.chatengine.io/users/',
+      {username: username, secret: username, first_name: username},
+      {headers: {"private-key": "b384d3b3-81ad-4193-a519-531c41a92f87"}}
+    )
+    return res.status(r.status).json(r.data)
+  } catch (e) {
+    return res.status(e.response.status).json(e.data)
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+app.listen(3001);
